f36789423326d885b0e9de1177689d13e9bd0a21
Jess <jessgatura@gmail.com>
2023-11-26 12:05:18 +0300

    updated teachers urls

--- teachers/urls.py
+++ teachers/urls.py
@@ -4,5 +4,5 @@ from .views import AddAttendance, AddParent, AddStudent
 urlpatterns = [
 	path('addParents/', AddParent.as_view(), name='add_parents'),
 	path('addStudents/', AddStudent.as_view(), name='add_students'),
-	path('attendance/', AddAttendance.as_view(), name='attendance'),
+	path('attendance/<int:teacher_id>/', AddAttendance.as_view(), name='attendance'),
 ]
